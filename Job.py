7acf327283f70d6843c03743205f4c82bbfbd91c
Koosha Saghari <ksaghari0@gmail.com>
2017-08-27 14:44:02 -0400

    Indentation fixes.

--- Job.py
+++ Job.py
@@ -1,15 +1,15 @@
 from sys import maxint
 
 class Job(object):
-"""
-__init__
+    """
+    __init__
 
-Initializer for a job instance
+    Initializer for a job instance
 
-@param startTime: Time at which the job entered the simulation
-@param processingTime: Processing time the job requires -> 0 if unspecified and to be set later
-@param mips: MIPS the jobs requires -> 0 if unspecified and to be set later
-"""
+    @param startTime: Time at which the job entered the simulation
+    @param processingTime: Processing time the job requires -> 0 if unspecified and to be set later
+    @param mips: MIPS the jobs requires -> 0 if unspecified and to be set later
+    """
     def __init__(self, startTime, processingTime=0, mips=0):
         super(Job, self).__init__()
         self.startTime = startTime
@@ -20,73 +20,73 @@ Initializer for a job instance
 
 # Getters
 
-"""
-getResponseTime
+    """
+    getResponseTime
 
-@return: response time for the job
-"""
+    @return: response time for the job
+    """
     def getResponseTime(self):
         return self.endTime - self.startTime
 
-"""
-getProcessingTime
+    """
+    getProcessingTime
 
-@return: processing time for the job
-"""
+    @return: processing time for the job
+    """
     def getProcessingTime(self):
         return self.processingTime
 
-"""
-getMIPS
+    """
+    getMIPS
 
-@return: MIPS required for the job
-"""
+    @return: MIPS required for the job
+    """
     def getMIPS(self):
         return self.requiredMIPS
 
 
-"""
-getIsFinished
+    """
+    getIsFinished
 
-@return: boolean determining if the job has been finished or not
-"""
+    @return: boolean determining if the job has been finished or not
+    """
     def getIsFinished(self):
         return self.isFinished
 
 # Setters
 
-"""
-setEndTime
+    """
+    setEndTime
 
-@param endTime: set the time at which the job ended
-@return: none
-"""
+    @param endTime: set the time at which the job ended
+    @return: none
+    """
     def setEndTime(self, endTime):
         self.endTime = endTime
 
-"""
-setProcessingTime
+    """
+    setProcessingTime
 
-@param procTime: set the processing time the job requires
-@return: none
-"""
+    @param procTime: set the processing time the job requires
+    @return: none
+    """
     def setProcessingTime(self, procTime):
         self.processingTime = procTime
 
-"""
-setMIPS
+    """
+    setMIPS
 
-@param mips: MIPS required for the job
-@return: none
-"""
+    @param mips: MIPS required for the job
+    @return: none
+    """
     def setMIPS(self, mips):
         self.requiredMIPS = mips
 
-"""
-setIsFinished
+    """
+    setIsFinished
 
-@param status: boolean T/F whether the job has been finished or not
-@return: none 
-"""
+    @param status: boolean T/F whether the job has been finished or not
+    @return: none
+    """
     def setIsFinished(self, status):
         self.isFinished = status
